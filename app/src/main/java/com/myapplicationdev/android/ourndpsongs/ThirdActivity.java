2c966c3d89891c1b68615af0e3d34518cfd2b2e8
James <20014241@myrp.edu.sg>
2021-08-05 21:59:35 +0800

    Fixed code

--- app/src/main/java/com/myapplicationdev/android/ourndpsongs/ThirdActivity.java
+++ app/src/main/java/com/myapplicationdev/android/ourndpsongs/ThirdActivity.java
@@ -1,22 +1,18 @@
 package com.myapplicationdev.android.ourndpsongs;
 
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RatingBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
 
 import sg.edu.rp.c346.id20014241.ourndpsongs.R;
 
@@ -51,17 +47,6 @@ public class ThirdActivity extends AppCompatActivity {
         etYear.setText(currentSong.getYearReleased()+"");
 
         ratingBar.setRating(currentSong.getStars());
-        /*switch (currentSong.getStars()){
-            case 5: rb5.setChecked(true);
-                    break;
-            case 4: rb4.setChecked(true);
-                    break;
-            case 3: rb3.setChecked(true);
-                    break;
-            case 2: rb2.setChecked(true);
-                    break;
-            case 1: rb1.setChecked(true);
-        }*/
 
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,9 +63,6 @@ public class ThirdActivity extends AppCompatActivity {
                 }
                 currentSong.setYearReleased(year);
 
-                //int selectedRB = rg.getCheckedRadioButtonId();
-                //RadioButton rb = (RadioButton) findViewById(selectedRB);
-                //currentSong.setStars(Integer.parseInt(rb.getText().toString()));
                 currentSong.setStars((int) ratingBar.getRating());
                 int result = dbh.updateSong(currentSong);
                 if (result>0){
@@ -95,84 +77,59 @@ public class ThirdActivity extends AppCompatActivity {
 
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                DBHelper dbh = new DBHelper(ThirdActivity.this);
-                int result = dbh.deleteSong(currentSong.getId());
-                if (result>0){
-                    Toast.makeText(ThirdActivity.this, "Song deleted", Toast.LENGTH_SHORT).show();
-                    finish();
-                } else {
-                    Toast.makeText(ThirdActivity.this, "Delete failed", Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View view) {
+                final DBHelper dbh = new DBHelper(ThirdActivity.this);
+
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(ThirdActivity.this);
+                myBuilder.setTitle("DANGER");
+                myBuilder.setMessage("Are you sure you want to delete the island \n" + etTitle.getText().toString());
+                myBuilder.setCancelable(false);
+
+                myBuilder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        int result = dbh.deleteSong(currentSong.getId());
+                        dbh.deleteSong(result);
+                        finish();
+                    }
+                });
+
+                myBuilder.setNeutralButton("Cancel", null);
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+
             }
         });
 
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(ThirdActivity.this);
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are you sure you want to discard the changes");
+                myBuilder.setCancelable(false);
+
+                myBuilder.setPositiveButton("Discard", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+
+                myBuilder.setNegativeButton("Do Not Discard", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+
             }
         });
 
     }
 
 
-    public static class CustomAdapter extends ArrayAdapter {
-        Context parent_context;
-        int layout_id;
-        ArrayList<com.myapplicationdev.android.ourndpsongs.Song> songs;
-
-        public CustomAdapter(Context context, int resource, ArrayList<com.myapplicationdev.android.ourndpsongs.Song> objects) {
-            super(context, resource, objects);
-            this.parent_context = context;
-            this.layout_id = resource;
-            this.songs = objects;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            // Obtain the LayoutInflater object
-            LayoutInflater inflater = (LayoutInflater)
-                    parent_context.getSystemService(LAYOUT_INFLATER_SERVICE);
-
-            // "Inflate" the View for each row
-            View rowView = inflater.inflate(layout_id, parent, false);
-
-            // Obtain the UI components and do the necessary binding
-            TextView tvTitle = rowView.findViewById(R.id.tvTitle);
-            TextView tvSingers = rowView.findViewById(R.id.tvSingers);
-            //TextView tvStars = rowView.findViewById(R.id.tvStars);
-            TextView tvYears = rowView.findViewById(R.id.tvYears);
-
-            ImageView imageView = rowView.findViewById(R.id.imageView);
-
-            RatingBar ratingBar = rowView.findViewById(R.id.ratingBar);
-
-            // Obtain the Android Version information based on the position
-            com.myapplicationdev.android.ourndpsongs.Song currentSong = songs.get(position);
-
-            // Set values to the TextView to display the corresponding information
-            tvTitle.setText(currentSong.getTitle());
-            tvSingers.setText(currentSong.getSingers());
-            String stars = "";
-            for(int i = 0; i < currentSong.getStars(); i++){
-                stars += " * ";
-            }
-            //tvStars.setText(stars);
-
-            ratingBar.setRating(currentSong.getStars());
-
-            tvYears.setText(currentSong.getYearReleased() + "");
-
-            if(currentSong.getYearReleased() >= 2019){
-                imageView.setImageResource(R.drawable.newsong);
-            }
-            else {
-                imageView.setVisibility(View.GONE);
-            }
-
-            return rowView;
-        }
-
-    }
 }
